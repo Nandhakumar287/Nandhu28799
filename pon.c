b704a233e5d2ec7122080c94f095f16e0f98ef2a
Nandha kumar S <mailstonandhakumar@gmail.com>
2017-06-28 14:25:26 +0530

    Create pon.c

---
+++ pon.c
@@ -0,0 +1,13 @@
+void main()
+{
+int a;
+prointf("\nEnter the number:");
+scanf("\n%d",&a);
+if(a>0)
+printf("\nPositive Number");
+else if(a<0)
+printf("\nNegative number");
+else
+printf("\nZero");
+getch();
+}
