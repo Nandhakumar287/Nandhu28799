0a8d43f226ab63dc2cd229827c975c58f0772ede
Nandha kumar S <mailstonandhakumar@gmail.com>
2017-06-29 20:24:49 +0530

    Create sumof.c

---
+++ sumof.c
@@ -0,0 +1,16 @@
+void main()
+{
+int num,i,sum=0;
+printf("\nEnter the range:");
+scanf("\n%d",&num);
+if(num>0){
+for(i=1;i<=num;i++)
+{
+sum=sum+1
+}
+printf("\n%d",sum);
+}
+else
+printf("Enter Zero plus values");
+getch();
+}
