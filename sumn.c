321f65f0990efc384ba081700647b4f76f01734c
Nandha kumar S <mailstonandhakumar@gmail.com>
2017-06-29 20:40:06 +0530

    Create sumn.c

---
+++ sumn.c
@@ -0,0 +1,16 @@
+void main()
+{
+int num,i,sum=0;
+printf("\nEnter the range:");
+scanf("\n%d",&num);
+if(num>=0){
+for(i=0;i<=num;i++)
+{
+sum=sum+1
+}
+printf("\nSum of N=%d",sum);
+}
+else
+printf("Enter Zero Numeric values");
+getch();
+}
