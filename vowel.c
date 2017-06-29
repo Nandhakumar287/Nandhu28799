5b7f5168f25549af6ec5c2528c203a0359253647
Nandha kumar S <mailstonandhakumar@gmail.com>
2017-06-29 19:11:43 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,11 @@
+void main()
+{
+char x;
+printf("\nEnter the char:");
+scanf("\n%d",&a);
+if(x=='a' || x=='e' ||x=='i' ||x=='o' ||x=='u')
+printf("\nchar is vowel");
+else
+printf("\nchar is consonant");
+getch();
+}
