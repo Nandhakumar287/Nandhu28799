8a2155ef0ccd19460ca60b31c49e496eb5420a84
Nandha kumar S <mailstonandhakumar@gmail.com>
2017-06-29 19:17:53 +0530

    Create chare.c

---
+++ chare.c
@@ -0,0 +1,11 @@
+void main()
+{
+char x;
+printf("\nEnter the char:");
+scanf("\n%c",&x);
+if((x<='a' && x>=z) || (x<='A' && x>='Z'))
+printf("\nchar is alphabate");
+else
+printf("\nchar is not alphabate");
+getch();
+}
